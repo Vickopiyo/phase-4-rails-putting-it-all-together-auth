3a289cd5207ec1fef7d9ed8445639dfc24a16d26
Vickopiyo <victor.okoth@student.moringaschool.com>
2022-10-07 15:36:38 +0300

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+
+  resources :recipes, only: [:index,:create]
+# singup
+  post "/signup", to: "users#create"
+  # maintain login
+  get "/me", to: "users#show"
+
+  # login
+  post "/login", to: "sessions#create"
+  # logout
+  delete "/logout", to: "sessions#destroy"
+  
+  # resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
